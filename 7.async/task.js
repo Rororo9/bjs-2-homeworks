4757616ed66e582eea786186f9277d859c1dd0cc
“Vadim” <freegasss@gmail.com>
2023-11-30 22:36:22 +1100

    Реализовано 7 задание

---
+++ 7.async/task.js
@@ -0,0 +1,64 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+
+        if (this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+
+        this.alarmCollection.push({
+            time: time,
+            callback: callback,
+            canCall: true,
+        });
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+
+
+    getCurrentFormattedTime() {
+        return new Date().toLocaleTimeString([], {
+            hour: '2-digit', minute: '2-digit'
+        });
+    }
+
+    start() {
+        if (this.intervalId) {
+            return this.intervalId;
+        }
+
+        this.intervalId = setInterval(() => {
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+        });
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => {
+            alarm.canCall = true;
+        });
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
